060dd361794700a4b464e44b02d45687de757f52
Alin-Stefan Ursu <aursu@deloittece.com>
2024-07-18 22:23:39 +0300

    Handle Git file rename

--- src/constant/gitConstants.ts
+++ src/constant/gitConstants.ts
@@ -2,6 +2,7 @@
 export const ADDITION = 'A'
 export const DELETION = 'D'
 export const MODIFICATION = 'M'
-export const GIT_DIFF_TYPE_REGEX = /^.\s+/u
+export const RENAME = 'R'
+export const GIT_DIFF_TYPE_REGEX = /^(.|R\d{1,4})\s+/u
 export const GIT_FOLDER = '.git'
 export const UTF8_ENCODING = 'utf8'
